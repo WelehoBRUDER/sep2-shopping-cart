f82053b5ca4a8cdda5f9304e8ffe22998f8e0f21
WelehoBRUDER <61124548+WelehoBRUDER@users.noreply.github.com>
2025-10-23 13:14:02 +0300

    Added unit tests

---
+++ src/test/java/TestShoppingCart.java
@@ -0,0 +1,27 @@
+import org.junit.jupiter.api.Test;
+
+import java.util.ResourceBundle;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class TestShoppingCart {
+    ResourceBundle messages = ResourceBundle.getBundle("messages");
+    ShoppingCart shoppingCart = new ShoppingCart(messages);
+
+    @Test
+    void testAddingItem() {
+        this.shoppingCart.items.clear();
+        this.shoppingCart.addItem(5, 4);
+        assertEquals(20, this.shoppingCart.items.get(0));
+        this.shoppingCart.addItem(59, 2.857);
+        assertEquals(168.563, this.shoppingCart.items.get(1), 0.001);
+    }
+
+    @Test
+    void testCalculatingAllItems() {
+        this.shoppingCart.items.clear();
+        this.shoppingCart.addItem(5, 4);
+        this.shoppingCart.addItem(59, 2.857);
+        assertEquals(188.563, this.shoppingCart.calculateTotalPrice(), 0.001);
+    }
+}
