9e1eadb168b999ac014cb86fd0fa2f32fd7e139a
WelehoBRUDER <61124548+WelehoBRUDER@users.noreply.github.com>
2025-10-23 12:59:56 +0300

    Refactor

---
+++ src/main/java/Main.java
@@ -0,0 +1,26 @@
+import java.util.Locale;
+import java.util.ResourceBundle;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        // Set language based on user input
+        System.out.print("Input language: ");
+        String language = sc.nextLine();
+        System.out.print("Input country: ");
+        String country = sc.nextLine();
+        Locale locale = new Locale(language, country);
+        ResourceBundle messages = ResourceBundle.getBundle("messages", locale);
+
+        // Create instance of shopping cart
+        ShoppingCart cart = new ShoppingCart(messages);
+
+        System.out.print(messages.getString("products_number") + ": ");
+        int num = sc.nextInt();
+        sc.nextLine();
+        cart.fillShoppingCart(num);
+        double total = cart.calculateTotalPrice();
+        System.out.println(messages.getString("total_price") + ": " + total);
+    }
+}
--- src/main/java/ShoppingCart.java
+++ src/main/java/ShoppingCart.java
@@ -1,39 +1,34 @@
-import java.util.Locale;
-import java.util.ResourceBundle;
-import java.util.Scanner;
+import java.util.*;
 
 public class ShoppingCart {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        // Set language based on user input
-        System.out.print("Input language: ");
-        String language = sc.nextLine();
-        System.out.print("Input country: ");
-        String country = sc.nextLine();
-        Locale locale = new Locale(language, country);
-        ResourceBundle messages = ResourceBundle.getBundle("messages", locale);
+    List<Double> items;
+    ResourceBundle messages;
+    Scanner sc = new Scanner(System.in);
 
-        System.out.print(messages.getString("products_number") + ": ");
-        int num = sc.nextInt();
-        sc.nextLine();
-        double[] cart = fillShoppingCart(num, sc, messages);
-        double total = calculateTotalPrice(cart);
-        System.out.println(messages.getString("total_price") + ": " + total);
+    public ShoppingCart(ResourceBundle messages) {
+        this.items = new ArrayList<Double>();
+        this.messages = messages;
     }
 
-    public static double[] fillShoppingCart(int size, Scanner sc, ResourceBundle messages) {
-        double[] cart = new double[size];
+    public double calculateItemCost(double amount, double price) {
+        return Math.floor(amount) * price;
+    }
+
+    public void fillShoppingCart(int size) {
         for (int i = 0; i < size; i++) {
-            System.out.print(messages.getString("item_count") + " (" + (i+1) + "): ");
+            System.out.print(messages.getString("item_count") + " (" + (i + 1) + "): ");
             double count = Double.parseDouble(sc.nextLine());
-            System.out.print(messages.getString("item_price") + " (" + (i+1) + "): ");
+            System.out.print(messages.getString("item_price") + " (" + (i + 1) + "): ");
             double price = Double.parseDouble(sc.nextLine());
-            cart[i] = Math.floor(count) * price;
+            this.addItem(count, price);
         }
-        return cart;
     }
 
-    public static double calculateTotalPrice(double[] items) {
+    public void addItem(double amount, double price) {
+        this.items.add(calculateItemCost(amount, price));
+    }
+
+    public double calculateTotalPrice() {
         double total = 0.0;
         for (double item : items) {
             total += item;
