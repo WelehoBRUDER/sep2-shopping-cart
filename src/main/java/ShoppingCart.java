f7e915cf45224634ec855aa1e7e831fdc2d319e2
WelehoBRUDER <61124548+WelehoBRUDER@users.noreply.github.com>
2025-10-23 12:30:50 +0300

    Added localization

--- src/main/java/ShoppingCart.java
+++ src/main/java/ShoppingCart.java
@@ -1,26 +1,33 @@
+import java.util.Locale;
+import java.util.ResourceBundle;
 import java.util.Scanner;
 
 public class ShoppingCart {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter the number of products: ");
+        // Set language based on user input
+        System.out.print("Input language: ");
+        String language = sc.nextLine();
+        System.out.print("Input country: ");
+        String country = sc.nextLine();
+        Locale locale = new Locale(language, country);
+        ResourceBundle messages = ResourceBundle.getBundle("messages", locale);
+
+        System.out.print(messages.getString("products_number") + ": ");
         int num = sc.nextInt();
         sc.nextLine();
-        double[][] cart = fillShoppingCart(num, sc);
+        double[][] cart = fillShoppingCart(num, sc, messages);
         double total = calculateTotalPrice(cart);
-        System.out.println("Total price is: " + total);
+        System.out.println(messages.getString("total_price") + ": " + total);
     }
 
-    public static double[][] fillShoppingCart(int size, Scanner sc) {
+    public static double[][] fillShoppingCart(int size, Scanner sc, ResourceBundle messages) {
         double[][] cart = new double[size][2];
         for (int i = 0; i < size; i++) {
-            System.out.print("Enter item count (" + (i+1) + "): ");
-            double count = sc.nextDouble();
-            sc.nextLine();
-            System.out.print("Enter item price (" + (i+1) + "): ");
-            double price = sc.nextDouble();
-            sc.nextLine();
-            System.out.println();
+            System.out.print(messages.getString("item_count") + " (" + (i+1) + "): ");
+            double count = Double.parseDouble(sc.nextLine());
+            System.out.print(messages.getString("item_price") + " (" + (i+1) + "): ");
+            double price = Double.parseDouble(sc.nextLine());
             cart[i][0] = Math.floor(count);
             cart[i][1] = price;
         }
