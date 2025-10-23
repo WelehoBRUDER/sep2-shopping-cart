83e5b14ac929d37c63df8ff6890cfc7163b0ca8e
WelehoBRUDER <61124548+WelehoBRUDER@users.noreply.github.com>
2025-10-23 12:47:43 +0300

    Fixed japan

--- src/main/java/ShoppingCart.java
+++ src/main/java/ShoppingCart.java
@@ -16,30 +16,27 @@ public class ShoppingCart {
         System.out.print(messages.getString("products_number") + ": ");
         int num = sc.nextInt();
         sc.nextLine();
-        double[][] cart = fillShoppingCart(num, sc, messages);
+        double[] cart = fillShoppingCart(num, sc, messages);
         double total = calculateTotalPrice(cart);
         System.out.println(messages.getString("total_price") + ": " + total);
     }
 
-    public static double[][] fillShoppingCart(int size, Scanner sc, ResourceBundle messages) {
-        double[][] cart = new double[size][2];
+    public static double[] fillShoppingCart(int size, Scanner sc, ResourceBundle messages) {
+        double[] cart = new double[size];
         for (int i = 0; i < size; i++) {
             System.out.print(messages.getString("item_count") + " (" + (i+1) + "): ");
             double count = Double.parseDouble(sc.nextLine());
             System.out.print(messages.getString("item_price") + " (" + (i+1) + "): ");
             double price = Double.parseDouble(sc.nextLine());
-            cart[i][0] = Math.floor(count);
-            cart[i][1] = price;
+            cart[i] = Math.floor(count) * price;
         }
         return cart;
     }
 
-    public static double calculateTotalPrice(double[][] items) {
+    public static double calculateTotalPrice(double[] items) {
         double total = 0.0;
-        for (double[] item : items) {
-            double amnt = item[0];
-            double price = item[1];
-            total += amnt * price;
+        for (double item : items) {
+            total += item;
         }
         return total;
     }
